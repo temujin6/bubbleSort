b237f13e8351b7277021b6c8c6cf5dbcb0012507
temujin <temvjintemvjin6@gmail.com>
2022-04-20 23:31:54 +0800

    bubble sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 function App() {
+  function bblSort(arr: any) {
+    for (var i = 0; i < arr.length; i++) {
+      for (var j = 0; j < arr.length - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          var temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+  }
+
+  var arr = [6, 4, 3, 6, 4, 11, 2, 5, 33, 5, 66, 44, 33];
+  const UnSortedArray = arr.map((number) => <li>{number}</li>);
+
+  bblSort(arr);
+
+  const SortedArray = arr.map((number) => <li>{number}</li>);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bubble sort</h1>
+      <div className="main">
+        <div className="first">
+          <h1>Эрэмбэлэгдээгүй массив</h1>
+          <ul>{UnSortedArray}</ul>
+        </div>
+        <div className="second">
+          <h1>Эрэмбэлэгдсэн массив</h1>
+          <ul>{SortedArray}</ul>
+        </div>
+      </div>
     </div>
   );
 }
